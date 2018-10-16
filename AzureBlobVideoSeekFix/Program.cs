b9edca5c18783852f1236374b8e6d2735e9ab1b7
Dave Mateer <davemateer@gmail.com>
2018-10-16 14:41:38 +0100

    init

--- AzureBlobVideoSeekFix/Program.cs
+++ AzureBlobVideoSeekFix/Program.cs
@@ -1,15 +1,22 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
 
 namespace AzureBlobVideoSeekFix
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            // to fix seek issue
+            //  https://blog.thoughtstuff.co.uk/2014/01/streaming-mp4-video-files-in-azure-storage-containers-blob-storage/
+            var credentials = new StorageCredentials("davemtestvideostorage", "INSERT KEY FROM AZURE WEB STORAGE KEYS HERE");
+            var account = new CloudStorageAccount(credentials, true);
+            var client = account.CreateCloudBlobClient();
+            var properties = client.GetServiceProperties();
+            properties.DefaultServiceVersion = "2013-08-15";
+            client.SetServiceProperties(properties);
+            Console.WriteLine(properties.DefaultServiceVersion);
         }
     }
 }
